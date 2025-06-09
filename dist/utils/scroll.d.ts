c2e01a99521145f95265a93268623c88a7e82ccb
Lukas Graf <graflukas218@gmail.com>
2025-06-09 09:55:58 +0200

    feat: add new scroll module to override scroll behaviour

---
+++ dist/utils/scroll.d.ts
@@ -0,0 +1,16 @@
+export interface DefaultScrollOptions {
+    defaultOffset: number;
+    defaultBehaviour: ScrollBehavior;
+}
+export interface OverrideScrollOptions {
+    offset: number;
+    behaviour: ScrollBehavior;
+}
+export declare const defaultScrollOptions: DefaultScrollOptions;
+export declare function scrollToSection(id: string, selectorType?: "id" | "any", options?: Partial<OverrideScrollOptions>): void;
+export declare function onScroll(event: Event, options?: Partial<DefaultScrollOptions>): void;
+export declare function initCMSScrollLinks(): void;
+export declare function initGlobalScrollLinks(): void;
+export declare function disableWebflowScroll(): void;
+export declare function overrideDefaultScroll(options?: Partial<DefaultScrollOptions>): void;
+export declare function overrideWebflowScroll(options?: Partial<DefaultScrollOptions>): void;
---
+++ src/utils/scroll.ts
@@ -0,0 +1,115 @@
+import createAttribute from "../attributeselector";
+
+export interface DefaultScrollOptions {
+  defaultOffset: number;
+  defaultBehaviour: ScrollBehavior;
+}
+
+export interface OverrideScrollOptions {
+  offset: number;
+  behaviour: ScrollBehavior;
+}
+
+export const defaultScrollOptions: DefaultScrollOptions = {
+  defaultOffset: 0,
+  defaultBehaviour: 'smooth'
+}
+
+export function scrollToSection(
+  id: string,
+  selectorType: "id" | "any" = "id",
+  options: Partial<OverrideScrollOptions> = {}
+): void {
+  const opts: OverrideScrollOptions = {
+    offset: options.offset ?? defaultScrollOptions.defaultOffset,
+    behaviour: options.behaviour ?? defaultScrollOptions.defaultBehaviour
+  }
+  setTimeout(() => {
+    const selector = selectorType === "id" ? `#${id}` : id;
+    const section = document.querySelector(selector);
+    if (section) {
+      const elementPosition = section.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.scrollY - opts.offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: opts.behaviour,
+      });
+    } else {
+      console.error(`Section with id '${id}' not found.`);
+    }
+  }, 10);
+}
+
+export function onScroll(event: Event, options: Partial<DefaultScrollOptions> = {}): void {
+  event.preventDefault();
+  const opts: DefaultScrollOptions = {
+    defaultOffset: options.defaultOffset ?? defaultScrollOptions.defaultOffset,
+    defaultBehaviour: options.defaultBehaviour ?? defaultScrollOptions.defaultBehaviour,
+  }
+  const link = event.target as HTMLAnchorElement;
+  if (!link) throw new Error(`Event target is undefined. Cannot scroll from an undefined link.`);
+  const scrollId =
+    link.getAttribute("href")?.slice(1) ||
+    link.getAttribute("scroll-to") ||
+    "";
+
+  const offset = parseInt(link.getAttribute("scroll-offset") || `${opts.defaultOffset}`, 10);
+  const behaviour = link.getAttribute("scroll-behaviour") as ScrollBehavior || opts.defaultBehaviour;
+  scrollToSection(scrollId, 'id', { offset, behaviour });
+}
+
+export function initCMSScrollLinks(): void {
+  const cmsScrollLinks = document.querySelectorAll<HTMLAnchorElement>(
+    "a[data-href-scroll]"
+  );
+  cmsScrollLinks.forEach((link) => {
+    const hrefPrefix = link.dataset.hrefPrefix || "";
+    const hrefScroll = link.dataset.hrefScroll || "";
+    link.href = `${hrefPrefix}#${hrefScroll}`;
+  });
+}
+
+export function initGlobalScrollLinks(): void {
+  const globalScrollLinks = document.querySelectorAll<HTMLAnchorElement>(
+    'a[data-global-scroll="true"]'
+  );
+
+  const globalFiltered = Array.from(globalScrollLinks).filter((link) => {
+    const url = new URL(link.href);
+    return url.pathname === location.pathname;
+  });
+
+  globalFiltered.forEach((link) => {
+    const url = new URL(link.href);
+    link.href = url.hash; // Set href to just the hash (e.g., "#section")
+  });
+}
+
+export function disableWebflowScroll(): void {
+  var Webflow = Webflow || [];
+  Webflow.push(function () {
+    $(function () {
+      $(document).off('click.wf-scroll');
+    });
+  });
+}
+
+export function overrideDefaultScroll(options: Partial<DefaultScrollOptions> = {}): void {
+  initCMSScrollLinks();
+  initGlobalScrollLinks();
+
+  const href = createAttribute('href');
+  const allScrollLinks = document.querySelectorAll<HTMLAnchorElement>(
+    `${href('#', { matchType: 'startsWith' })}, [scroll-to]`
+  );
+
+  allScrollLinks.forEach((link) => {
+    link.addEventListener("click", (event) => onScroll(event, options));
+  });
+}
+
+export function overrideWebflowScroll(options: Partial<DefaultScrollOptions> = {}): void {
+  disableWebflowScroll();
+  overrideDefaultScroll(options);
+}
