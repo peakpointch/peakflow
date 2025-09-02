25d3a78f8f4deef85b88f03c5d6d62ce8d96464f
Lukas Manuel Graf <graflukas218@gmail.com>
2025-09-02 22:48:21 +0200

    feat(typeutils): add BooleanString and StringifyBoolean, add docs

--- dist/typeutils/index.d.ts
+++ dist/typeutils/index.d.ts
@@ -1,3 +1,14 @@
 export type CamelToDash<T extends string> = T extends `${infer Head}${infer Tail}` ? Head extends Lowercase<Head> ? `${Head}${CamelToDash<Tail>}` : `-${Lowercase<Head>}${CamelToDash<Tail>}` : T;
 export type DashToCamelCase<T extends string> = T extends `${infer Head}-${infer Tail}` ? `${Head}${Capitalize<DashToCamelCase<Tail>>}` : T;
+/**
+ * Make all properties in T optional, except those of type K
+ */
 export type PartialExcept<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+/**
+ * Boolean values as a string.
+ */
+export type BooleanString = "true" | "false";
+/**
+ * Replace `true` and `false` values with their string versions.
+ */
+export type StringifyBoolean<T> = Exclude<T, boolean> | BooleanString;
--- src/typeutils/index.ts
+++ src/typeutils/index.ts
@@ -8,4 +8,17 @@ export type DashToCamelCase<T extends string> = T extends `${infer Head}-${infer
   ? `${Head}${Capitalize<DashToCamelCase<Tail>>}` // Capitalize the first character of Tail
   : T;
 
+/**
+ * Make all properties in T optional, except those of type K
+ */
 export type PartialExcept<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+
+/**
+ * Boolean values as a string.
+ */
+export type BooleanString = "true" | "false";
+
+/**
+ * Replace `true` and `false` values with their string versions.
+ */
+export type StringifyBoolean<T> = Exclude<T, boolean> | BooleanString;
