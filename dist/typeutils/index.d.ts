2b596d5997b1f4891b904081b6be617dd6f5dc56
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-25 14:00:41 +0100

    feat(typeutils): add CamelToPascal, PascalToCamel, DashToPascal, PascalToDash

--- dist/typeutils/index.d.ts
+++ dist/typeutils/index.d.ts
@@ -1,5 +1,9 @@
 export type CamelToDash<T extends string> = T extends `${infer Head}${infer Tail}` ? Head extends Lowercase<Head> ? `${Head}${CamelToDash<Tail>}` : `-${Lowercase<Head>}${CamelToDash<Tail>}` : T;
 export type DashToCamelCase<T extends string> = T extends `${infer Head}-${infer Tail}` ? `${Head}${Capitalize<DashToCamelCase<Tail>>}` : T;
+export type CamelToPascal<T extends string> = T extends `${infer Head}${infer Tail}` ? `${Uppercase<Head>}${Tail}` : T;
+export type PascalToCamel<T extends string> = T extends `${infer Head}${infer Tail}` ? `${Lowercase<Head>}${Tail}` : T;
+export type DashToPascal<T extends string> = Capitalize<DashToCamelCase<T>>;
+export type PascalToDash<T extends string> = CamelToDash<PascalToCamel<T>>;
 /**
  * Make all properties in T optional, except those of type K
  */
