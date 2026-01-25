e32d31088417cdc7b78ea925009227a4e252de88
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-25 22:57:55 +0100

    refactor(swiper)!: reorganize & implement new attribute definition method
    - use the Dataset.define() method to define the dataset
    - this approach replaces the old parseDataset method, which was less
      maintainable and not as intuitive (annoying to be clear)
    
    BREAKING CHANGE
    - functions isEmpty, hideEmptySlider, initCounter, updateCounter,
      initSlides are now static methods of the Slider class

--- dist/swiper/swiper.d.ts
+++ dist/swiper/swiper.d.ts
@@ -1,8 +1,30 @@
+import { type Attribute, type Attributes } from "../selector/index.js";
 import { Stylesheet } from "../utils";
+import type { CamelToDash } from "../typeutils";
 import Swiper from "swiper";
-import type { SwiperOptions } from "swiper/types";
+import type { AutoplayOptions, NavigationOptions, SwiperOptions } from "swiper/types";
 import { BaseComponent, type BaseSettings } from "../base-component/index.js";
 type SliderElement = "component" | "wrapper" | "controls" | "navigation" | "pagination" | "prev" | "next" | "counter-current" | "counter-separator" | "counter-total";
+type SliderHideOptions = "hideNone" | "hideComponent" | "emptyState";
+type SliderAttribute = "data-swiper-id" | "data-swiper-element" | "data-swiper-hide-options" | `data-${CamelToDash<keyof SwiperOptions>}` | `data-nav-${CamelToDash<keyof NavigationOptions>}` | `data-autoplay-${CamelToDash<keyof AutoplayOptions>}`;
+interface SwiperAttributes extends Attributes<string, SliderAttribute> {
+    id: Attribute<"data-swiper-id", string>;
+    element: Attribute<"data-swiper-element", SliderElement>;
+    hide: Attribute<"data-swiper-hide-options", SliderHideOptions>;
+    allowTouchMove: Attribute<"data-allow-touch-move", boolean>;
+    autoHeight: Attribute<"data-auto-height", boolean>;
+    autoplay: Attribute<"data-autoplay", boolean>;
+    autoplayDelay: Attribute<"data-autoplay-delay", number>;
+    centeredSlides: Attribute<"data-centered-slides", boolean>;
+    freeMode: Attribute<"data-free-mode", boolean>;
+    followFinger: Attribute<"data-follow-finger", boolean>;
+    loop: Attribute<"data-loop", boolean>;
+    mousewheel: Attribute<"data-mousewheel", boolean>;
+    slidesPerView: Attribute<"data-slides-per-view", "auto" | number>;
+    slideToClickedSlide: Attribute<"data-slide-to-clicked-slide", boolean>;
+    spaceBetween: Attribute<"data-space-between", number>;
+    speed: Attribute<"data-speed", number>;
+}
 export interface SliderSettings extends BaseSettings {
     options: SwiperOptions;
 }
@@ -41,11 +63,7 @@ export interface SliderSettings extends BaseSettings {
 export declare class Slider extends BaseComponent<SliderElement> {
     swiper: Swiper;
     stylesheet: Stylesheet;
-    static attr: {
-        id: string;
-        element: string;
-        hide: string;
-    };
+    static attr: import("../selector/attributes.js").AttributeAccessorMap<SwiperAttributes>;
     constructor(component: HTMLElement, settings: BaseSettings);
     protected static readonly attributeSelector: import("../selector/selector.js").AttributeSelector<string>;
     static selector: import("../selector/selector.js").InstanceSelector<SliderElement>;
@@ -54,5 +72,10 @@ export declare class Slider extends BaseComponent<SliderElement> {
     private static create;
     static initAll(container?: HTMLElement, options?: SwiperOptions): void;
     static readOptions(swiperElement: HTMLElement, override?: SwiperOptions): SwiperOptions;
+    static isEmpty(slider: HTMLElement): boolean;
+    static hideEmptySlider(slider: HTMLElement): void;
+    static updateCounter(swiper: Swiper, currentElement: HTMLElement, totalElement: HTMLElement): void;
+    static initCounter(swiper: Swiper): void;
+    static initSlides(wrapperEl: HTMLElement): void;
 }
 export {};
