32f03c9963e9b8534ebdd5545d0dd58c020b4d3c
Lukas Manuel Graf <graflukas218@gmail.com>
2026-01-26 14:35:25 +0100

    fix(selector): ParsedAttributes doesn't accept Attributes as a type parameter

--- dist/selector/attributes.d.ts
+++ dist/selector/attributes.d.ts
@@ -12,10 +12,10 @@ export type DatasetAttributes<T extends Attributes = Attributes> = {
     [K in keyof T]: T[K] extends Attribute<infer N, infer U> ? IsUnknown<U> extends false ? DatasetAttribute<N, U> : DatasetAttribute<N, string> : never;
 };
 export type AttributeType<T = string> = (val: string | null | undefined, attr: DatasetAttribute<string, T>) => T;
-export type ParsedAttributes<T extends DatasetAttributes> = {
+export type ParsedAttributes<T extends Attributes> = {
     [K in keyof T]: ParsedAttribute<T[K]>;
 };
-export type ParsedAttribute<T extends DatasetAttribute> = undefined extends T["default"] ? IsUnknown<T> extends false ? ParsedAttributeValue<T> : string : NonNullable<ParsedAttributeValue<T>>;
+export type ParsedAttribute<T extends Attribute> = T extends DatasetAttribute ? undefined extends T["default"] ? IsUnknown<T> extends false ? ParsedAttributeValue<T> : string : NonNullable<ParsedAttributeValue<T>> : never;
 export type ParsedAttributeValue<T extends DatasetAttribute> = T["type"] extends AttributeType<infer U> ? U : never;
 export type AttributeAccessorMap<T extends Attributes = Attributes> = {
     [K in keyof T]: T[K] extends Attribute<infer N, infer _> ? N : never;
@@ -27,8 +27,8 @@ export declare class Dataset<T extends Attributes> {
     static define<T extends Attributes<keyof T & string>>(attributes: T): Dataset<T>;
     static defineAttribute<N extends string>(attr: N): DatasetAttribute<N, string>;
     static defineAttribute<N extends string, T>(attr: Attribute<N, T>): DatasetAttribute<N, T>;
-    static parse<T extends DatasetAttributes<any>>(element: Element, attributes: T): ParsedAttributes<T>;
-    parse(element: Element): ParsedAttributes<DatasetAttributes<T>>;
+    static parse<T extends Attributes>(element: Element, attributes: DatasetAttributes<T>): ParsedAttributes<T>;
+    parse(element: Element): ParsedAttributes<T>;
     static getAttribute(element: Element, attr: string): string;
     static getAttribute<T>(element: Element, attr: Attribute<string, T>): T;
     static String<T extends string, N extends string = string>(name: N, defaultValue?: T): DatasetAttribute<N, T>;
