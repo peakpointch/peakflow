13af643debe598d0be79131bd8abd34cdcc69ed1
Lukas Manuel Graf <graflukas218@gmail.com>
2026-05-07 20:22:06 +0200

    refactor(pdf)!: outsource file config, remove reliance on globals
    BREAKING CHANGE:
    - File config needs to be passed in to `.preview(file)`

--- types/peakflow.d.ts
+++ types/peakflow.d.ts
@@ -1,10 +1,8 @@
-import type { PdfEmbedGlobal } from "../src/pdf";
 import type { Webflow } from "../src/webflow";
 
 export type PeakflowShared = Record<string, any>;
 
 export interface PeakflowGlobal {
-  pdfEmbed: PdfEmbedGlobal;
   webflow: Webflow;
   shared: PeakflowShared;
 }
