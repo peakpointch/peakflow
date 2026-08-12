5229150e5f0a18ee70c3e45598af36ab45c48a2e
Lukas Manuel Graf <graflukas218@gmail.com>
2026-08-12 07:18:11 +0200

    feat(typeutils): add Expect and Equal

--- dist/typeutils/index.d.ts
+++ dist/typeutils/index.d.ts
@@ -22,3 +22,5 @@ export type StringTypeMap = {
     number: number;
     numberOrAuto: number | "auto";
 };
+export type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false;
+export type Expect<T extends true> = T;
--- src/typeutils/index.ts
+++ src/typeutils/index.ts
@@ -42,4 +42,12 @@ export type StringTypeMap = {
   boolean: boolean;
   number: number;
   numberOrAuto: number | "auto";
-};
+}
+
+export type Equal<A, B> =
+  (<T>() => T extends A ? 1 : 2) extends
+  (<T>() => T extends B ? 1 : 2)
+    ? true
+    : false;
+
+export type Expect<T extends true> = T;;
